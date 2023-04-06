93e4be436d08c6f2d2bd0f48a5d9e8e384c65c52
Weston Sandfort <sandfortw@gmail.com>
2023-04-06 16:35:18 -0600

    refactoring complete

--- app/poros/merchant.rb
+++ app/poros/merchant.rb
@@ -5,4 +5,11 @@ class Merchant
     @id = merchant_data[:id]
     @name = merchant_data[:attributes][:name]
   end
+
+  def items
+    json = MerchantService.new.merchant_items(@id)
+    json[:data].map do |item_data|
+      Item.new(item_data)
+    end
+  end
 end
---
+++ app/services/merchant_service.rb
@@ -0,0 +1,19 @@
+class MerchantService
+
+  def all_merchants
+    @all_merchants ||= get_url('http://localhost:3000/api/v1/merchants')
+  end
+
+  def one_merchant(id)
+    get_url("http://localhost:3000/api/v1/merchants/#{id}")
+  end
+
+  def merchant_items(id)
+    get_url("http://localhost:3000/api/v1/merchants/#{id}/items")
+  end
+
+  def get_url(url)
+    response = Faraday.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
