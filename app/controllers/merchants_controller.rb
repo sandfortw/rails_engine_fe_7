2aa3eda4469c7d617cc31173b4a3f374f4943f41
Weston Sandfort <sandfortw@gmail.com>
2023-04-06 10:56:29 -0600

    complete the challenge in a dumb way

---
+++ app/controllers/merchants_controller.rb
@@ -0,0 +1,23 @@
+class MerchantsController < ApplicationController
+
+  def index
+    response = Faraday.get("http://localhost:3000/api/v1/merchants")
+    json = JSON.parse(response.body, symbolize_names: true)
+    @merchants = json[:data].map do |merchant_data|
+      Merchant.new(merchant_data)
+    end
+  end
+
+  def show
+    require 'pry'; binding.pry
+    merchant_response = Faraday.get("http://localhost:3000/api/v1/merchants/#{params[:id]}")
+    require 'pry'; binding.pry
+    json = JSON.parse(merchant_response.body, symbolize_names: true)
+    @merchant = Merchant.new(json[:data])
+    item_response = Faraday.get("http://localhost:3000/api/v1/merchants/#{params[:id]}/items")
+    json = JSON.parse(item_response.body, symbolize_names: true)
+    @items = json[:data].map do |item_data|
+      Item.new(item_data)
+    end
+  end
+end
